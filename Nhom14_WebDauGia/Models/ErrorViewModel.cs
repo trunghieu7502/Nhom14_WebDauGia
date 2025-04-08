3689daafae713448e7515787b68cd0f538fd235c
trunghieu7502 <hieu5393@gmail.com>
2025-04-08 10:45:52 +0700

    Add project files.

---
+++ Nhom14_WebDauGia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nhom14_WebDauGia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
